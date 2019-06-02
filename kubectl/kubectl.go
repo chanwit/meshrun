9e207486f2cc6a1a4c6e30f9d889d00efaf0df86
Chanwit Kaewkasi <chanwit@gmail.com>
2019-06-02 20:29:24 +0700

    implement kubectl wrapper

---
+++ kubectl/kubectl.go
@@ -0,0 +1,23 @@
+package kubectl
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func Apply(yaml string) error {
+	cmd := exec.Command("kubectl", "apply", "-f", "-")
+	cmd.Stdin = strings.NewReader(yaml)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func Delete(yaml string) error {
+	cmd := exec.Command("kubectl", "delete", "-f", "-")
+	cmd.Stdin = strings.NewReader(yaml)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
