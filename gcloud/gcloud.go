7b134aa9a853edfd643f2f449a1fda6ae2e7fa18
Chanwit Kaewkasi <chanwit@gmail.com>
2019-06-02 20:42:00 +0700

    implement registering a cloud run service as a gloo upstream

--- gcloud/gcloud.go
+++ gcloud/gcloud.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
+	"github.com/chanwit/meshrun/kubectl"
 	servingv1alpha1 "github.com/knative/serving/pkg/apis/serving/v1alpha1"
+	"sigs.k8s.io/yaml"
 )
 
 func DeployV1alpha1Service(svc servingv1alpha1.Service) {
@@ -41,6 +44,66 @@ func DeployV1alpha1Service(svc servingv1alpha1.Service) {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
+
+	RegisterUpstream(svc)
+}
+
+func RegisterUpstream(svc servingv1alpha1.Service) error {
+
+	args := []string{"beta", "run", "services", "describe",
+		"--namespace=" + svc.ObjectMeta.Namespace,
+		"--region=" + svc.ObjectMeta.Labels["cloud.googleapis.com/location"],
+		svc.ObjectMeta.Name,
+	}
+
+	cmd := exec.Command("gcloud", args...)
+	// cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	output, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+
+	var outputSvc servingv1alpha1.Service
+	err = yaml.Unmarshal([]byte(output), &outputSvc)
+	if err != nil {
+		return err
+	}
+
+	url := outputSvc.Status.Address.Hostname
+	parts := strings.SplitN(url, "//", 2)
+	if len(parts) != 2 {
+		return fmt.Errorf("URL format is invalid: %s", url)
+	}
+
+	hostname := parts[1]
+	name := svc.ObjectMeta.Name
+	namespace := svc.ObjectMeta.Namespace
+
+	template := `
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: %s
+---
+apiVersion: gloo.solo.io/v1
+kind: Upstream
+metadata:
+  labels:
+    service: %s
+  name: %s
+  namespace: %s
+spec:
+  discoveryMetadata: {}
+  upstreamSpec:
+    static:
+      hosts:
+      - addr: %s
+        port: 443
+`
+
+	return kubectl.Apply(fmt.Sprintf(template, namespace, name, name, namespace, hostname))
 }
 
 func DeleteV1alpha1Service(svc servingv1alpha1.Service) {
